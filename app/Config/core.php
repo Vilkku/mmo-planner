b3abc8a701a7009cb83c52971953fc50eec80bd2
Vilkku <vilkku@gmail.com>
2013-09-08 15:56:16 +0300

    Add DebugKit, do initial config

--- app/Config/core.php
+++ app/Config/core.php
@@ -33,7 +33,7 @@
  * In production mode, flash messages redirect after a time interval.
  * In development mode, you need to click the flash message to continue.
  */
-	Configure::write('debug', 2);
+	Configure::write('debug', 1);
 
 /**
  * Configure the Error handler used to handle errors for your application. By default
@@ -224,12 +224,12 @@
 /**
  * A random string used in security hashing methods.
  */
-	Configure::write('Security.salt', 'DYhG93b0qyJfIxfs2guVoUubWwvniR2G0FgaC9mi');
+	Configure::write('Security.salt', 'C94tk5CmuTzS5JhYzIe3');
 
 /**
  * A random numeric string (digits only) used to encrypt/decrypt strings.
  */
-	Configure::write('Security.cipherSeed', '76859309657453542496749683645');
+	Configure::write('Security.cipherSeed', '43140892669142630237');
 
 /**
  * Apply timestamps with the last modified time to static assets (js, css, images).
